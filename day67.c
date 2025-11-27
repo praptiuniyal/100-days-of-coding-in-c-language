0e6f08458a2c37c05e6c12479bda1ecc51400458
praptiuniyal <uniyalprapti8@gmail.com>
2025-11-27 22:47:54 +0530

    Add files via upload

---
+++ day67.c
@@ -0,0 +1,53 @@
+// Write a program to take two sorted arrays of size m and n as input. Merge both the arrays such that the merged array is also sorted. Print the merged array.
+
+
+#include <stdio.h>
+
+int main()
+ {
+    int m, n;
+    scanf("%d",&m);
+    int arr1[m];
+    for (int i = 0; i < m; i++)
+     {
+        scanf("%d", &arr1[i]);
+    }
+
+    scanf("%d",&n);
+    int arr2[n];
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr2[i]);
+    }
+
+    int merged[m + n];
+    int i = 0, j = 0, k = 0;
+
+    while (i< m && j < n) 
+    {
+        if (arr1[i] <= arr2[j])
+         {
+            merged[k++] = arr1[i++];
+        } else {
+            merged[k++] = arr2[j++];
+        }
+    }
+
+    while (i< m)
+     {
+        merged[k++] = arr1[i++];
+    }
+
+    while (j< n)
+     {
+        merged[k++] = arr2[j++];
+    }
+
+    for (int x = 0; x < m + n; x++) 
+    {
+        printf("%d ", merged[x]);
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ day69.c
@@ -0,0 +1,31 @@
+// Write a program to take an integer array as input. Only one element will be repeated. Print the repeated element. Try to find the result in one single iteration.
+
+#include <stdio.h>
+
+int main()
+ {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    int seen[n+1];   
+    for (int i= 0;i<= n;i++) 
+    {
+        seen[i]= 0;
+    }
+
+    int repeated = -1;
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+        if (seen[arr[i]] == 1)
+         {
+            repeated= arr[i];
+            break;   
+        }
+        seen[arr[i]] = 1;
+    }
+
+    printf("%d\n",repeated);
+    return 0;
+}
